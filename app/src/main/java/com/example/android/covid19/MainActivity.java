b70808d9b7aa1aeb57ecc3f3101825e562d249c6
rajanjais4 <rajanjais4.com>
2020-06-06 18:27:14 +0530

    learn to switch working om rj layout

--- app/src/main/java/com/example/android/covid19/MainActivity.java
+++ app/src/main/java/com/example/android/covid19/MainActivity.java
@@ -13,16 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ct=(Button)findViewById(R.id.ct);
-        st=(Button)findViewById(R.id.st);
-        dt=(Button)findViewById(R.id.dt);
 
-        ct.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                
-            }
-        });
+
 
 
     }
