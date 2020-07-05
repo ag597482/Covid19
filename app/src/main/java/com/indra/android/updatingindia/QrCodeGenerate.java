3ace3afccd83ed36fdbce1e7dee128bd4e80cff7
rajanjais4 <rajanjais4@gmail.com>
2020-07-05 18:01:43 +0530

    scan almost to fire

--- app/src/main/java/com/indra/android/updatingindia/QrCodeGenerate.java
+++ app/src/main/java/com/indra/android/updatingindia/QrCodeGenerate.java
@@ -1,16 +1,25 @@
 package com.indra.android.updatingindia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
@@ -18,7 +27,16 @@ import com.google.zxing.qrcode.QRCodeWriter;
 
 public class QrCodeGenerate extends AppCompatActivity {
 
+
+    String user_mail;
+    String user_id;
+    FirebaseAuth mauth;
+    FirebaseUser user;
+    DatabaseReference dataref;
+    String constant="Indra_co";
+    String input_location;
     String text_to_qr="Indra_co";
+
     private ImageView imageView;
     private EditText location_text;
     int width=200,hight=200;
@@ -27,6 +45,11 @@ public class QrCodeGenerate extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mauth=FirebaseAuth.getInstance();
+        user=mauth.getCurrentUser();
+        user_mail=user.getEmail();
+        user_id=user_mail.replace(".",",");
+        dataref= FirebaseDatabase.getInstance().getReference().child("users");
         setContentView(R.layout.activity_qr_code_generate);
         imageView=findViewById(R.id.qr_image);
 
@@ -43,10 +66,14 @@ public class QrCodeGenerate extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                text_to_qr=location_text.getText().toString();
-                if(text_to_qr.isEmpty())
+                input_location=location_text.getText().toString();
+                if(input_location.isEmpty())
                 {
                     Toast.makeText(QrCodeGenerate.this, "You Must fill the location name", Toast.LENGTH_SHORT).show();
+                }
+                else if(input_location.contains("."))
+                {
+                    Toast.makeText(QrCodeGenerate.this, "location name can NOT have Dot", Toast.LENGTH_SHORT).show();
                 }
                 else {
                     button.setVisibility(View.GONE);
@@ -57,22 +84,47 @@ public class QrCodeGenerate extends AppCompatActivity {
             }
         });
     }
+
+
     public void QRCodeButton(View view)
     {
-        QRCodeWriter qrCodeWriter=new QRCodeWriter();
-        try {
-            BitMatrix bitMatrix=qrCodeWriter.encode(text_to_qr, BarcodeFormat.QR_CODE,width,hight);
-            Bitmap bitmap=Bitmap.createBitmap(width,hight,Bitmap.Config.RGB_565);
-            for (int x = 0; x<200; x++){
-                for (int y=0; y<200; y++){
-                    bitmap.setPixel(x,y,bitMatrix.get(x,y)? Color.BLACK : Color.WHITE);
+        FirebaseDatabase.getInstance().getReference().child("globle").child("globle num").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                long num=dataSnapshot.getValue(long.class);
+                FirebaseDatabase.getInstance().getReference().child("globle").child("globle num").setValue(num+1);
+                QRCodeWriter qrCodeWriter=new QRCodeWriter();
+                try {
+
+                    text_to_qr =input_location+"@"+text_to_qr+"@"+num;
+                    BitMatrix bitMatrix=qrCodeWriter.encode(text_to_qr, BarcodeFormat.QR_CODE,width,hight);
+                    Bitmap bitmap=Bitmap.createBitmap(width,hight,Bitmap.Config.RGB_565);
+                    for (int x = 0; x<200; x++){
+                        for (int y=0; y<200; y++){
+                            bitmap.setPixel(x,y,bitMatrix.get(x,y)? Color.BLACK : Color.WHITE);
+                        }
+                    }
+                    imageView.setImageBitmap(bitmap);
+
+                    ScanGenFireData qrdata=new ScanGenFireData();
+                    qrdata.setLocation_name(input_location);
+                    qrdata.setQr_num(num);
+                    dataref.child(user_id).child("qr generated").child(input_location).setValue(qrdata);
+
+                    ScanHistory.scan_history_infos_array.add(new scan_history_info(location_text.getText().toString()));
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
+
             }
-            imageView.setImageBitmap(bitmap);
-            ScanHistory.scan_history_infos_array.add(new scan_history_info(location_text.getText().toString()));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 
     public void QRCodeButton_Intent()
---
+++ app/src/main/java/com/indra/android/updatingindia/ScanGenFireData.java
@@ -0,0 +1,22 @@
+package com.indra.android.updatingindia;
+
+public class ScanGenFireData {
+    private String location_name;
+    private long qr_num;
+
+    public String getLocation_name() {
+        return location_name;
+    }
+
+    public void setLocation_name(String location_name) {
+        this.location_name = location_name;
+    }
+
+    public long getQr_num() {
+        return qr_num;
+    }
+
+    public void setQr_num(long qr_num) {
+        this.qr_num = qr_num;
+    }
+}
