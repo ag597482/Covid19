1c23a916c9fcaace7ad3e753aa47ec201a37fdc0
ag597482 <ag597482@gmai.com>
2020-07-11 16:17:20 +0530

    listner added

--- app/src/main/java/com/indra/android/updatingindia/Assest.java
+++ app/src/main/java/com/indra/android/updatingindia/Assest.java
@@ -1,5 +1,6 @@
 package com.indra.android.updatingindia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,23 +9,39 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Assest extends AppCompatActivity {
 
+    String user_mail;
+    String user_id;
 
-
-    TextView nameid,reportno,reportdetails;
+    TextView nameid,reportno,reportdetails,fetch;
     Button submitreport;
 
     private FirebaseDatabase firebaseDatabase;
-    private DatabaseReference databaseReference;
+    private DatabaseReference databaseReference,userref,shopref;
 
+    FirebaseAuth mauth;
+    FirebaseUser user;
+
+    Map<String,String> startd,endd;
+    ArrayList<String> list;
+
+    private ValueEventListener eventListener,eventListener1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +52,24 @@ public class Assest extends AppCompatActivity {
         reportno =findViewById(R.id.rno);
         reportdetails = findViewById(R.id.rd);
         submitreport = findViewById(R.id.submittest);
+        fetch = findViewById(R.id.fet);
 
+        startd = new HashMap<>();
+        endd = new HashMap<>();
+
+        list = new ArrayList<>();
 
         firebaseDatabase=FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference().child("tests");
+
+
+        mauth= FirebaseAuth.getInstance();
+        user=mauth.getCurrentUser();
+        user_mail=user.getEmail();
+        user_id=user_mail.replace(".",",");
+
+        userref = firebaseDatabase.getReference().child("users").child(user_id).child("qr scan history list");
+        shopref = firebaseDatabase.getReference().child("globe").child("qr location");
 
 
 
@@ -60,17 +91,91 @@ public class Assest extends AppCompatActivity {
                     Calendar calendar=Calendar.getInstance();
                     DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
                     String time= df.format(calendar.getTime());
-                    Report report = new Report(nameid.getText().toString().trim(),reportno.getText().toString().trim(),time);
+                    Report report = new Report(user_id,reportno.getText().toString().trim(),time);
 
 
-                    databaseReference.child(nameid.getText().toString().trim()).setValue(report);
+                    databaseReference.child(user_id).setValue(report);
 
-                    Toast.makeText(getBaseContext(), "Test submitted ..!", Toast.LENGTH_SHORT).show();
-                    finish();
+
+                    eventListener = new ValueEventListener() {
+
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                            startd.clear();
+                            endd.clear();
+                            for (DataSnapshot ds : dataSnapshot.getChildren()) {
+
+                                String shopname = ds.getValue(String.class);
+
+
+
+                                for(DataSnapshot d : ds.getChildren())
+                                {
+
+                                    String entryt = d.child("entry time").getValue(String.class);
+                                    String exitt = d.child("exit time").getValue(String.class);
+
+                                    startd.put(shopname,entryt);
+                                    endd.put(shopname,exitt);
+
+
+
+                                    list.clear();
+                                    eventListener1 = new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                                            String usersid = dataSnapshot.getValue(String.class);
+
+                                            list.add(usersid);
+
+
+
+                                        }
+
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                        }
+                                    };
+
+                                    shopref.child(shopname).child("scan history").addValueEventListener(eventListener1);
+
+
+                                }
+
+
+                            }
+
+                            fetch.setText("Fetching - > "+  startd.toString());
+
+
+                        }
+
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    };
+                    userref.addValueEventListener(eventListener);
+
+
+                    ////
+                   // Toast.makeText(getBaseContext(), "Test submitted ..! " + list.toString() + startd.toString() , Toast.LENGTH_SHORT).show();
+
+
                 }
             }
         });
 
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        userref.removeEventListener(eventListener);
     }
 }
